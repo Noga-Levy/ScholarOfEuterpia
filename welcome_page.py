8d0e9e107a6956a518857c1e6a65be7286d741a4
Noga Levy <nogalevy17@gmail.com>
2025-08-01 19:21:47 -0700

    Added streamlit and text to welcome_page.py

---
+++ welcome_page.py
@@ -0,0 +1,34 @@
+"""
+Entry page to the game
+"""
+
+import streamlit as st
+
+st.markdown("<h1 style='text-align: center;'>Welcome, Scholar!</h1>", unsafe_allow_html=True)
+
+st.markdown("<h2 style='text-align: center;'>The world of Euterpia has been plunged into darkness--can you "
+            "save it?</h2>", unsafe_allow_html=True)
+
+st.markdown("<h4 style='text-align: center;'>A long, long time ago, Euterpia was a blissful land of song and "
+            "dance... but that was before the darkness arrived</h4>", unsafe_allow_html=True)
+
+st.markdown("<h4 style='text-align: center;'>It destroyed the musician's notes</h4>", unsafe_allow_html=True)
+st.markdown("<h4 style='text-align: center;'>It destroyed the composer's theory</h4>", unsafe_allow_html=True)
+st.markdown("<h4 style='text-align: center;'>And worst of all, it destroyed Euterpia's music</h4>",
+            unsafe_allow_html=True)
+
+st.markdown("<h4 style='text-align: center;'>Luckily, a brave group of people stood up against this force, "
+            "and defeated it. They called themselves the Scholars.</h4>", unsafe_allow_html=True)
+
+st.markdown("<h4 style='text-align: center;'>And, a millennium later, Euterpia has been restored to its former glory"
+            "</h4>", unsafe_allow_html=True)
+
+st.markdown("<h4 style='text-align: center;'> </h4>", unsafe_allow_html=True)
+
+st.markdown("<h4 style='text-align: center;'>... well, almost.</h4>", unsafe_allow_html=True)
+
+st.markdown("<h2 style='text-align: center;'>There's still one missing piece that's keeping the land from achieving "
+            "true harmony. The Scholars have sent their best--that's you!--to go retrieve it.</h2>",
+            unsafe_allow_html=True)
+
+st.markdown("<h2 style='text-align: center;'>Are you ready for an adventure?</h2>", unsafe_allow_html=True)
