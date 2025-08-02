83da647a63cea14ba4ad1a28cd606e925d7f2985
Noga Levy <nogalevy17@gmail.com>
2025-08-01 21:21:34 -0700

    Cutscene #1 of level 1

---
+++ level1.py
@@ -0,0 +1,45 @@
+"""
+LEVEL 1
+
+The Taxi
+"""
+
+import streamlit as st
+import time
+
+st.markdown("<h1 style='text-align: center;'>Level 1</h1>", unsafe_allow_html=True)
+st.markdown("<h2 style='text-align: center;'>The C A B</h2>", unsafe_allow_html=True)
+
+
+def type_writer(msg: str, character: str, avtr):
+    def delayed_stream(text):
+        for char in text:
+            yield char
+            time.sleep(0.05)
+
+    person = st.chat_message(character, avatar=avtr)
+
+    return person.write_stream(delayed_stream(msg))
+
+
+type_writer("To the docks, please", "You", "the_Scholar_avatar.png")
+
+type_writer("Sure", "Taxi Man", "taxi_man.png")
+type_writer(".    ", "Taxi Man", "taxi_man.png")
+type_writer(".    ", "Taxi Man", "taxi_man.png")
+type_writer(".    ", "Taxi Man", "taxi_man.png")
+type_writer("Wait a minute... are YOU the Scholar everyone's been makin' a fuss about?", "Taxi Man",
+            "taxi_man.png")
+
+type_writer("Er... Probably? I'm on a quest to retrieve the final piece the darkness destroyed",
+            "You", "the_Scholar_avatar.png")
+
+type_writer("Huh. I'll be honest, I'm not impressed. You don't LOOK like a butt-kickin' Scholar.",
+            "Taxi Man", "taxi_man.png")
+
+type_writer("Well... we have a while till we arrive. How about this: ask me some questions about music theory, "
+            "and let's see how many I get right.",
+            "You", "the_Scholar_avatar.png")
+
+type_writer("Hmf. You've got your self a deal!", "Taxi Man", "taxi_man.png")
+
