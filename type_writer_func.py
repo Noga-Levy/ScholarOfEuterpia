4458761def1cf225a90a7a1f1af4f2bf33e27edf
Noga Levy <nogalevy17@gmail.com>
2025-08-03 16:21:33 -0700

    Added a special write_stream for the narrator/narration. And the smaller time sleep is temporary, I just added it during testing because I'm impatient.

--- type_writer_func.py
+++ type_writer_func.py
@@ -6,8 +6,17 @@ def type_writer(msg: str, character: str, avtr):
     def delayed_stream(text):
         for char in text:
             yield char
-            time.sleep(0.05)
+            time.sleep(0.005)
 
     person = st.chat_message(character, avatar=avtr)
 
     return person.write_stream(delayed_stream(msg))
+
+
+def narator(msg: str):
+    def delayed_stream(text):
+        for char in text:
+            yield char
+            time.sleep(0.05)
+
+    return st.write_stream(delayed_stream(msg))
